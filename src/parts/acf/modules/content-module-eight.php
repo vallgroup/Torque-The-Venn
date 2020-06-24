c1235550baa76531830d7d70d9f80b5a57305796
Al Notara <al@vallgroup.com>
2020-06-25 09:50:56 +1000

    fixed undefined var warning in gallery template

--- src/parts/acf/modules/content-module-eight.php
+++ src/parts/acf/modules/content-module-eight.php
@@ -13,9 +13,7 @@ $use_lightbox = get_field( 'filtered_gallery_use_lightbox', $filtered_gallery_id
   : '';
 ?>
 
-<div class="content-module-eight-container
-  <?php echo $alignment_style_class; ?>
-">
+<div class="content-module-eight-container">
   <div class="content-wrapper">
     <?php if ( $filtered_gallery_id ) { 
       echo do_shortcode( '[torque_filtered_gallery gallery_id="' . $filtered_gallery_id . '" hide_filters="' . $hide_filters . '" use_lightbox="' . $use_lightbox . '"]' );
